f39ef83ae469c9f0dc338f4f8b6a6403c2e10030
imtiaz782 <imtiazak782@gmail.com>
2024-08-13 16:43:40 +0500

    sum of two no

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello_World!")
+    int a = 4;
+    int b = 6;
+    printf("The sum of a %d and b %d is %d \n",a,b,a+b);
+    printf("Hello_World!");
     return 0;
 }
